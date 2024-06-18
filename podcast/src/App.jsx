f6d054c6c92db041a210c73135a9b97d3c833390
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-18 17:48:39 +0200

    renamed files

--- podcast/src/App.jsx
+++ podcast/src/App.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import { useState } from 'react'
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "../utils/Themes";
 import './App.css'
 
+const Container = styled.div`
+background: ${({ theme }) => theme.bg };
+width: 100%;
+height: 100vh;
+`;
+
 function App() {
-  
+  //hooks
+  const  [darkMode,setDarkMode] = useState (true);
+
   return (
     <>
-      <div>
-        podcast
-      </div>
+    <ThemeProvider theme={darkTheme}>
+
+    <Container>
+      Podcast
+    </Container>
+  
+    </ThemeProvider>
     </>
-  )
+  ); 
 }
 
-export default App
+export default App;
