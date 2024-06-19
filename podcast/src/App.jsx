ced119dda0ac7f9dafb296180b1251c5c5021b93
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-19 15:36:47 +0200

    "Updated App.jsx and Sidebar.jsx with minor formatting changes, added MainContent component, and imported Sidebar component in App.jsx"

--- podcast/src/App.jsx
+++ podcast/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
+import { useState } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "../utils/Themes";
-import './App.css'
+import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 const Container = styled.div`
-display: flex;
-background: ${({ theme }) => theme.bg };
-width: 100%;
-height: 100vh;
-overflow-x: hidden;
-overflow-y: hidden;
+  display: flex;
+  background: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100vh;
+  overflow-x: hidden;
+  overflow-y: hidden;
+`;
+const MainContent = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${({ theme }) => theme.text};
 `;
 
 function App() {
   //hooks
-  const  [darkMode,setDarkMode] = useState (true);
+  const [darkMode, setDarkMode] = useState(true);
 
   return (
     <>
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-
-    <Container>
-      <>
-      Sidebar
-      </>
-      Podcast
-    </Container>
-  
-    </ThemeProvider>
+      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+        <Container>
+          <Sidebar />
+          <MainContent>Podcast</MainContent>
+        </Container>
+      </ThemeProvider>
     </>
-  ); 
+  );
 }
 
 export default App;
--- podcast/src/components/Sidebar.jsx
+++ podcast/src/components/Sidebar.jsx
@@ -1,26 +1,26 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
+import { HomeRounded, CloseRounded } from "@mui/icons-material";
 
-const MenuContainer = styled.div`
-
-`;
+const MenuContainer = styled.div``;
 const Logo = styled.div``;
 const Close = styled.div``;
 const Elements = styled.div``;
 const NavText = styled.div``;
 
 const Sidebar = () => {
-  return <MenuContainer>
-
-        <Logo></Logo>
-        <Close>
-                <CloseRounded/>
-        </Close>
-        <Elements>
-                <HomeRounder/>
-                <NavText>Dashboard</NavText>
-        </Elements>
-        </MenuContainer>;
+  return (
+    <MenuContainer>
+      <Logo></Logo>
+      <Close>
+        <CloseRounded />
+      </Close>
+      <Elements>
+        <HomeRounded />
+        <NavText>Dashboard</NavText>
+      </Elements>
+    </MenuContainer>
+  );
 };
 
 export default Sidebar;
