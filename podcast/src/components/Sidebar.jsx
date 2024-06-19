a306783c8ac05f22c622eb17798bb97a7dc2f1c8
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-19 16:06:20 +0200

    Added style to the menu Container and updated the dependencies for the coded to run properly

--- podcast/src/components/Sidebar.jsx
+++ podcast/src/components/Sidebar.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import { HomeRounded, CloseRounded } from "@mui/icons-material";
 
-const MenuContainer = styled.div``;
+const MenuContainer = styled.div`
+flex: 0.5;
+flex-direction: column;
+height: 100vh;
+display: flex;
+background-color: ${({ theme }) => theme.bg};
+
+`;
 const Logo = styled.div``;
 const Close = styled.div``;
 const Elements = styled.div``;
