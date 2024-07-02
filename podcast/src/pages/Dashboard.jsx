65734f649a7334bbb67df9772be3e79ede93763d
Omphile Morwane <omphilemorwane@gmail.com>
2024-07-02 09:35:30 +0200

    Edited the sliding corousel

--- podcast/src/pages/Dashboard.jsx
+++ podcast/src/pages/Dashboard.jsx
@@ -7,34 +7,39 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
-
 const sliderSettings = {
   dots: true,
   infinite: true,
-  speed: 500,
-  slidesToShow: 3,
-  slidesToScroll: 1,
+  speed: 800,
+  slidesToShow: 4,
+  slidesToScroll: 4,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        
       },
     },
     {
-      breakpoint: 600,
+      breakpoint: 768,
       settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        
       },
     },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        
+      }
+    }
   ],
 };
-
-
 
 const PlayIcon = styled.div`
   padding: 10px;
@@ -236,6 +241,24 @@ const Podcasts = styled.div`
   }
 `;
 
+const SliderContainer = styled.div`
+  .slick-slider {
+  width: 100%;
+}
+
+
+  .slick-slide {
+    display: inline-block;
+    padding: 0 10px;
+    box-sizing: border-box;
+  }
+
+  .slick-track {
+    display: flex;
+    align-items: center;
+  }
+`;
+
 const DisplayPodcast = ({ podcast }) => (
   <Card>
     <div>
@@ -291,10 +314,7 @@ const Dashboard = () => {
       <FilterContainer>
         <Topic>
           Podcasts
-          <Link
-            to={`/showpodcasts/podcasts`}
-            style={{ textDecoration: "none" }}
-          >
+          <Link to={`/showpodcasts/podcast`} style={{ textDecoration: "none" }}>
             <Span>Show All</Span>
           </Link>
         </Topic>
@@ -308,18 +328,19 @@ const Dashboard = () => {
       <FilterContainer>
         <Topic>
           Recommended
-          <Link
-            to={`/showpodcasts/recommended`}
-            style={{ textDecoration: "none" }}
-          >
+          <Link to={`/showpodcasts/podcast`} style={{ textDecoration: "none" }}>
             <Span>Show All</Span>
           </Link>
         </Topic>
-        <Slider {...sliderSettings}>
-          {recommended.map((podcast) => (
+        <Podcasts>
+          <SliderContainer>
+          <Slider {...sliderSettings}>
+             {recommended.map((podcast) => (
             <DisplayPodcast key={podcast.id} podcast={podcast} />
           ))}
         </Slider>
+        </SliderContainer>
+          </Podcasts>   
       </FilterContainer>
     </DashboardMain>
   );
